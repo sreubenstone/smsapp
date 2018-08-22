f2b6fc892eac271b7734dd619e874f4f50c3f195
Steven Reubenstone <Crossover@Stevens-MacBook-Air-3.local>
2018-08-22 16:34:27 -0400

    New push trying to figure out postgres

--- server.ts
+++ server.ts
@@ -1,13 +1,22 @@
 const http = require('http');
 const express = require('express');
+var path = require('path');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
+var bodyParser = require('body-parser');
+var pg = require('pg');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
 app.post('/sms', (req, res) => {
+
+  var msgFrom = req.body.From;
+
   const twiml = new MessagingResponse();
 
-  twiml.message('The Robots are coming! Head for the hills!');
+  twiml.message('Thank you for messaging The Pick Up Pen Network. Please submit a pick up line by typing submit then the pick up line or type get to receive a random pick up line tip. or ');
 
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
@@ -16,3 +25,9 @@ app.post('/sms', (req, res) => {
 http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
 });
+
+/* DB Work */
+
+var connect = "postgres://crossover:password@locahost/pendb"
+
+pg.connect
