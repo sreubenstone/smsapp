062c3aae5d7f01646aa8268a7df5e92e561425b5
Ranjan Dailata <ranjancse@gmail.com>
2018-08-24 22:11:42 -0500

    Re-factored code

--- actions/get.ts
+++ actions/get.ts
@@ -5,22 +5,22 @@ const { Client } = require('pg');
 export default class GetAction{
 
 	async handleGetAction(req: express.Request, res: express.Response){
-      const client = new Client();
-      await client.connect();
-      let sql = 'INSERT INTO users (phoneid) VALUES ($1) ON CONFLICT (phoneid) DO NOTHING';
-      let params = [req.body.From];
-      let result = await client.query(sql, params);
+            const client = new Client();
+            await client.connect();
+            let sql = 'INSERT INTO users (phoneid) VALUES ($1) ON CONFLICT (phoneid) DO NOTHING';
+            let params = [req.body.From];
+            let result = await client.query(sql, params);
 
-      sql = 'UPDATE users SET getrequests = getrequests + 1 where phoneid = $1';
-      params = [req.body.From];
-      result = await client.query(sql, params);
+            sql = 'UPDATE users SET getrequests = getrequests + 1 where phoneid = $1';
+            params = [req.body.From];
+            result = await client.query(sql, params);
 
-      sql = 'SELECT * FROM messages ORDER BY random() LIMIT 1';
-      result = await client.query(sql, null);
-    
-      const twiml = new MessagingResponse();
-      twiml.message(result);
-      res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
+            sql = 'SELECT * FROM messages ORDER BY random() LIMIT 1';
+            result = await client.query(sql, null);
+          
+            const twiml = new MessagingResponse();
+            twiml.message(result);
+            res.writeHead(200, {'Content-Type': 'text/xml'});
+            res.end(twiml.toString());
 	}
 }
--- actions/submit.ts
+++ actions/submit.ts
@@ -4,22 +4,20 @@ const { Client } = require('pg');
 
 export default class SubmitAction{
 	async handleSubmitAction(req: express.Request, res: express.Response){
-   
-      const twiml = new MessagingResponse();
-      twiml.message('You have succesfully inserted your pick up line to the network.');
-      res.writeHead(200, {'Content-Type': 'text/xml'});
-      res.end(twiml.toString());
+            const twiml = new MessagingResponse();
+            twiml.message('You have succesfully inserted your pick up line to the network.');
+            res.writeHead(200, {'Content-Type': 'text/xml'});
+            res.end(twiml.toString());
 
-      const client = new Client();
-      await client.connect();
-      let sql = 'INSERT INTO users (phoneid) VALUES ($1) ON CONFLICT (phoneid) DO NOTHING';
-      let params = [req.body.From];
-      let result = await client.query(sql, params);
+            const client = new Client();
+            await client.connect();
+            let sql = 'INSERT INTO users (phoneid) VALUES ($1) ON CONFLICT (phoneid) DO NOTHING';
+            let params = [req.body.From];
+            let result = await client.query(sql, params);
 
-      sql = 'INSERT INTO messages (userid, message) VALUES ($1, $2)';
-      params = [req.body.From, req.body.Body];
-      result = await client.query(sql, params);
-      console.log('Inserted', result);
-
+            sql = 'INSERT INTO messages (userid, message) VALUES ($1, $2)';
+            params = [req.body.From, req.body.Body];
+            result = await client.query(sql, params);
+            console.log('Inserted', result);
 	}
 }
